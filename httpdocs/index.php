c9649e695fe5df29dae612c3f820112ee936526d
Ruben de Vries <rubensayshi@gmail.com>
2011-05-22 23:07:34 +0200

    added hrefs for extra commands

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,3 +1,7 @@
+<div style="float: left; border: 1px solid black; width: 1022px; height: 50px; padding: 10px;">
+	client side: <a href="spam.php">spam the server with 1000 creates</a> | <a href="clean.php">clean everything</a>
+
+</div>
 <div style="float: left; border: 1px solid black; padding: 10px;">
 	<h3>C L I E N T</h3>
 	<iframe src="client.app.php" width="500" height="800" frameborder="0"></iframe>
